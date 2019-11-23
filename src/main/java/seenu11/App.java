3e3d3d5c7a1f4d69306ffb1e6822a4b99c02ce4d
praveen <praveen51.rymec@gmail.com>
2019-11-23 19:20:28 +0530

    mavendemo

---
+++ src/main/java/seenu11/App.java
@@ -0,0 +1,13 @@
+package seenu11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/seenu11/demoTest.java
@@ -0,0 +1,21 @@
+package seenu11;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+	@Test
+public void bottletest()
+{
+	System.out.println("welcome to bottletest");	
+}
+	@Test
+	public void chargertest()
+	{
+		System.out.println("welcome to chargertest");
+	}
+	@Test
+	public void remotetest()
+	{
+		System.out.println("welcome to remotetest");
+	}
+}
